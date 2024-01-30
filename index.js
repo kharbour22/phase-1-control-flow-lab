d85d40b1a92e1f9e4097c37de9b760f595aab99c
KevinHarbour <kharbour22@gmail.com>
2024-01-29 19:33:26 -0500

    donit

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet < 400) {
+    return 'This one is on me!';
+}
+else if (feet >= 400 && feet <= 2000) {
+  return 'That will be twenty bucks.'
+}
+ else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+}
+ else if (feet > 2500) {
+    return 'No can do.';
+}
+}
+  // Write your code here!
+
+function ternaryCheckCity(city){
+  return (city === 'NYC' ? "Ok, sounds good." : "No go.")
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+      break;
   // Write your code here!
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
